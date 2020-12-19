b42c5eda2f4863523e2c43e0dd4ccc159fcfee1b
felix t <41817193+o2bomb@users.noreply.github.com>
2020-12-19 23:38:25 +0800

    Ignore node_modules/ when watching files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const isDevelopment = process.env.NODE_ENV === "development";
 
 module.exports = {
   entry: "./src/index.ts",
@@ -9,11 +10,23 @@ module.exports = {
         test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/
-      }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+        exclude: /node_modules/
+      },
     ]
   },
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  watchOptions: {
+    ignored: 'node_modules/**'
   },
   output: {
     filename: "bundle.js",
